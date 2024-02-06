464fb1cbfe061ad20d607bbd00b301ba48831d61
brunoosuna8 <brunoosuna8@gmail.com>
2024-02-06 16:30:56 -0300

    add:supplier crud and entity

--- src/supplier/entities/supplier.entity.ts
+++ src/supplier/entities/supplier.entity.ts
@@ -1,1 +1,15 @@
-export class Supplier {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Supplier {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+  @Column()
+  name: string;
+  @Column()
+  phone: number;
+  @Column()
+  email: string;
+  @Column()
+  address: string;
+}
--- src/supplier/supplier.module.ts
+++ src/supplier/supplier.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { SupplierService } from './supplier.service';
-import { SupplierController } from './supplier.controller';
+import { Module } from "@nestjs/common";
+import { SupplierService } from "./supplier.service";
+import { SupplierController } from "./supplier.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Supplier } from "./entities/supplier.entity";
 
 @Module({
   controllers: [SupplierController],
   providers: [SupplierService],
+  imports: [TypeOrmModule.forFeature([Supplier])],
 })
 export class SupplierModule {}
