c486229afe9478c17b962bf778dac64122ccfd03
brunoosuna8 <86745936+brunoosuna8@users.noreply.github.com>
2024-02-07 15:33:41 -0500

    add:user relation with role (#17)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 import { User } from "./entities/user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Role } from "src/role/entities/role.entity";
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Role])],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,16 +8,22 @@ import { UpdateUserDto } from "./dto/update-user.dto";
 import { User } from "./entities/user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { Role } from "src/role/entities/role.entity";
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>
   ) {}
   async create(createUserDto: CreateUserDto) {
     try {
       const user = this.userRepository.create(createUserDto);
+      user.role = await this.roleRepository.findOneBy({
+        id: createUserDto.roleId,
+      });
       await this.userRepository.save(user);
       return user;
     } catch (error) {
@@ -38,7 +44,10 @@ export class UserService {
 
   async findOne(id: number) {
     try {
-      const user = await this.userRepository.findOne({ where: { id } });
+      const user = await this.userRepository.findOne({
+        where: { id },
+        relations: { role: true },
+      });
       if (!user) {
         throw new NotFoundException(`Error Get user by id ${id}`);
       }
@@ -52,10 +61,13 @@ export class UserService {
   async update(id: number, updateUserDto: UpdateUserDto) {
     try {
       const user = await this.userRepository.findOne({ where: { id } });
+      user.role = await this.roleRepository.findOneBy({
+        id: updateUserDto.roleId,
+      });
       if (!user) {
-        throw new NotFoundException(`Error update user by id ${id}`);
+        throw new NotFoundException(`Error expeption update user by id ${id}`);
       }
-      await this.userRepository.update(id, updateUserDto);
+      await this.userRepository.save(user);
       return true;
     } catch (error) {
       console.log(error);
