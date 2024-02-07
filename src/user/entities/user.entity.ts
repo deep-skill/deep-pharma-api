7c8c1c406937f1ca029fb9574be9b66261e917c6
brunoosuna8 <brunoosuna8@gmail.com>
2024-02-07 16:34:37 -0300

    add:user entity, dto and crud

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+  @Column("text")
+  name: string;
+  @Column("text")
+  lastName: string;
+  @Column("int", {
+    unique: true,
+  })
+  dni: number;
+  @Column()
+  password: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,79 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { User } from "./entities/user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createUserDto);
+      await this.userRepository.save(user);
+      return user;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException("Error creating user");
+    }
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    try {
+      const users = await this.userRepository.find();
+      return users;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException("Error Get all  users");
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    try {
+      const user = await this.userRepository.findOne({ where: { id } });
+      if (!user) {
+        throw new NotFoundException(`Error Get user by id ${id}`);
+      }
+      return user;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException("Error getting user");
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    try {
+      const user = await this.userRepository.findOne({ where: { id } });
+      if (!user) {
+        throw new NotFoundException(`Error update user by id ${id}`);
+      }
+      await this.userRepository.update(id, updateUserDto);
+      return true;
+    } catch (error) {
+      console.log(error);
+      throw new NotFoundException(`Error update user by id ${id}`);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    try {
+      const user = await this.userRepository.findOne({ where: { id } });
+      if (!user) {
+        throw new NotFoundException(`Error remove user by id ${id}`);
+      }
+      await this.userRepository.delete(id);
+      return true;
+    } catch (error) {
+      console.log(error);
+      throw new NotFoundException(`Error remove user by id ${id}`);
+    }
   }
 }
