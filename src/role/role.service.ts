8ee2f8834a81d0375f1f186b6c9d7ebcbbd3b2b2
Manuel Mandujano <manduuu2294@gmail.com>
2024-02-07 14:43:18 -0500

    update entity role (#15)

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,26 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Role } from './entities/role.entity';
 
 @Injectable()
 export class RoleService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  
+  constructor (
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository <Role>
+  ) {}
+  
+  async create(createRoleDto: CreateRoleDto) {
+    try {
+      const role = this.roleRepository.create( createRoleDto );
+      await this.roleRepository.save( role );
+      return role;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException('Error creating role')
+    }
   }
 
-  findAll() {
-    return `This action returns all role`;
+  async findAll() {
+    try {
+      const roles = await this.roleRepository.find();
+      return roles;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException('Error Get all role')
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: number) {
+    try {
+      const role = await this.roleRepository.findOne({ where: { id } });
+      if (!role) {
+        throw new NotFoundException(`Error Get brands by id ${id}`)
+      }
+      return role;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException('Error Get role')
+    }
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    try {
+      const role = await this.roleRepository.findOne({ where: { id } });
+      if (!role) {
+        throw new NotFoundException(`Error Get brands by id ${id}`)
+      }
+      await this.roleRepository.update(id, updateRoleDto);
+      return true;
+    } catch (error) {
+      console.log(error)
+      throw new NotFoundException(`Error Get roles by id ${id}`)
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: number) {
+    try {
+      const role = await this.roleRepository.findOne({ where: { id } });
+      if (!role) {
+        throw new NotFoundException(`Error Get roles by id ${id}`)
+      }
+      await this.roleRepository.delete(id);
+      return true;
+    } catch (error) {
+      console.log(error)
+      throw new NotFoundException(`Error Get roles by id ${id}`)
+    }
   }
 }
