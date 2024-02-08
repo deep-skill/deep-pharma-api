999b2019cbe70d316df4e2c3e5947ae1b0d98585
brunoosuna8 <brunoosuna8@gmail.com>
2024-02-08 11:35:19 -0300

    add:customer entity,crud

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,1 +1,11 @@
-export class Customer {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Customer {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+  @Column()
+  phone_number: number;
+  @Column()
+  name: string;
+}
