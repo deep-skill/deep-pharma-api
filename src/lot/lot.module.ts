64b24178117ac2e5645b9e7bf02040f37890c55a
Franco Galbiati <francogalbiati984@gmail.com>
2024-02-08 11:19:28 -0500

    added create and edit more relationship between product and lot

--- src/lot/lot.module.ts
+++ src/lot/lot.module.ts
@@ -4,12 +4,13 @@ import { LotController } from './lot.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lot } from './entities/lot.entity';
 import { Supplier } from 'src/supplier/entities/supplier.entity';
+import { Product } from 'src/product/entities/product.entity';
 
 @Module({
   controllers: [LotController],
   providers: [LotService],
   imports: [
-    TypeOrmModule.forFeature([Lot, Supplier]),
+    TypeOrmModule.forFeature([Lot, Supplier, Product]),
   ],
 })
 export class LotModule { }
