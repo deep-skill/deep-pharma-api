9c9ea0ba340cede87c480a1d7512329337b0b6a1
Franco Galbiati <francogalbiati984@gmail.com>
2024-02-15 12:03:29 -0500

    create entity in database

--- src/price_product_recommended/price_product_recommended.module.ts
+++ src/price_product_recommended/price_product_recommended.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PriceProductRecommendedService } from './price_product_recommended.service';
 import { PriceProductRecommendedController } from './price_product_recommended.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PriceProductRecommended } from './entities/price_product_recommended.entity';
 
 @Module({
   controllers: [PriceProductRecommendedController],
   providers: [PriceProductRecommendedService],
+  imports: [
+    TypeOrmModule.forFeature([ PriceProductRecommended ]),
+  ],
 })
 export class PriceProductRecommendedModule {}
--- src/price_product_recommended/price_product_recommended.service.ts
+++ src/price_product_recommended/price_product_recommended.service.ts
@@ -1,19 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePriceProductRecommendedDto } from './dto/create-price_product_recommended.dto';
 import { UpdatePriceProductRecommendedDto } from './dto/update-price_product_recommended.dto';
+import { PriceProductRecommended } from './entities/price_product_recommended.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PriceProductRecommendedService {
-  create(createPriceProductRecommendedDto: CreatePriceProductRecommendedDto) {
-    return 'This action adds a new priceProductRecommended';
+
+  constructor(
+    @InjectRepository( PriceProductRecommended )
+    private readonly priceRepository: Repository<PriceProductRecommended>
+  ) {}
+  async create(createPriceProductRecommendedDto: CreatePriceProductRecommendedDto) {
+    try {
+      const price = this.priceRepository.create( createPriceProductRecommendedDto );
+      await this.priceRepository.save( price );
+      return price;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException(error)
+    }
   }
 
-  findAll() {
-    return `This action returns all priceProductRecommended`;
+  async findAll() {
+    try {
+      const prices = await this.priceRepository.find();
+      return prices;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException(error)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} priceProductRecommended`;
+  async findOne(id: number) {
+    try {
+      const price = await this.priceRepository.findOne({ where: { id } });
+      if (!price) {
+        throw new NotFoundException(`Error Get brands by id ${id}`)
+      }
+      return price;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException(error)
+    }
   }
 
   update(id: number, updatePriceProductRecommendedDto: UpdatePriceProductRecommendedDto) {
