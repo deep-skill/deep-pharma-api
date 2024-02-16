45955856a1e595e5417baffc454b76c081122ea2
Franco Galbiati <francogalbiati984@gmail.com>
2024-02-16 13:41:39 -0500

    change supplier

--- src/supplier/dto/create-supplier.dto.ts
+++ src/supplier/dto/create-supplier.dto.ts
@@ -1,18 +1,24 @@
-import { IsEmail, IsNumberString, IsString, MaxLength, MinLength } from "class-validator";
+import { IsEmail, IsNumber, IsNumberString, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateSupplierDto {
   @IsString()
   @MinLength(3)
   @MaxLength(50)
   name: string;
+
   @IsString()
   @MinLength(3)
   @MaxLength(50)
   address: string;
+
   @IsNumberString()
   @MinLength(6)
   @MaxLength(11)
   phone_number: number;
+
   @IsEmail()
   email: string;
+
+  @IsNumber()
+  created_by: number
 }
