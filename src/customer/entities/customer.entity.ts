39ddd6ab3e6342babd33ac91db5518938c8dcff9
Franco Galbiati <francogalbiati984@gmail.com>
2024-02-16 14:29:03 -0500

    add customer changes

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,11 +1,14 @@
+import { ClassGlobal } from "src/class_global/class_global.entity";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class Customer {
+export class Customer extends ClassGlobal {
   @PrimaryGeneratedColumn("increment")
   id: number;
+
   @Column()
   phone_number: number;
+  
   @Column()
   name: string;
 }
